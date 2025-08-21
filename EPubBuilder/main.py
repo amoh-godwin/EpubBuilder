97047fc23aa169945be32901e6cd102d603b159e
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 12:34:43 +0000

    Update main.py

--- EPubBuilder/main.py
+++ EPubBuilder/main.py
@@ -5,6 +5,5 @@ from Builder import Builder
 
 os.sep = "/"
 
-builder = Builder("tests/epubs")
-container_name = builder.make_container_folder("first_epub")
-print(container_name)
+builder = Builder("../tests/epubs")
+builder.make_container_folder("first_epub")
