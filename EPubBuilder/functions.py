48dc9cf2d8ebd90b6771bab4864ebc985ce3da51
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 18:03:34 +0000

    Update functions.py

--- EPubBuilder/functions.py
+++ EPubBuilder/functions.py
@@ -1,6 +1,9 @@
 import os
 
 
+TEMPLATE_FOLDER = "./templates"
+
+
 def make_mimetype_file(folder: str) -> str:
     """
     Creates a mimetype file in the specified folder.
@@ -38,84 +41,14 @@ def make_container_xml(folder: str) -> bool:
 
 def make_opf_file(folder: str, title: str) -> str:
 
-    opf_content = """
-    <?xml version="1.0"?>
-    <package
-    version="3.0"
-    xml:lang="en"
-    xmlns="http://www.idpf.org/2007/opf"
-    unique-identifier="pub-id">
+    opf_template_file = os.path.join(TEMPLATE_FOLDER, 'title.opf')
+    with open(opf_template_file, 'r') as opf_f:
+        template_content = opf_f.read()
 
-   <metadata
-       xmlns:dc="http://purl.org/dc/elements/1.1/">
-
-      <dc:identifier
-          id="pub-id">
-         urn:uuid:B9B412F2-CAAD-4A44-B91F-A375068478A0
-      </dc:identifier>
-
-      <dc:language>
-         en
-      </dc:language>
-
-      <dc:title>
-         Book Title
-      </dc:title>
-
-      <dc:creator
-          id="creator">
-         Creator Name
-      </dc:creator>
-
-      <meta
-          property="dcterms:modified">
-         2000-03-24T00:00:00Z
-      </meta>
-
-      <dc:publisher>
-         Book Publisher
-      </dc:publisher>
-
-      <dc:date>
-         2000-01-31
-      </dc:date>
-
-      <meta
-          property="dcterms:dateCopyrighted">
-         9999-01-01
-      </meta>
-
-      <dc:identifier
-          id="isbn13">
-         urn:isbn:9780741014559
-      </dc:identifier>
-
-      <dc:identifier
-          id="isbn10">
-         0-7410-1455-6
-      </dc:identifier>
-   </metadata>
-
-   <manifest>
-      <item id="r4915"
-          href="book.html"
-          media-type="application/xhtml+xml"/>
-      <item id="r7184"
-          href="images/cover.png"
-          media-type="image/png"/>
-      <item id="nav"
-          href="nav.html"
-          media-type="application/xhtml+xml"
-          properties="nav"/>
-   </manifest>
-
-   <spine>
-      <itemref
-          idref="r4915"/>
-   </spine>
-</package>
-    """
+    opf_content = template_content.replace("{{title}}", title)
 
     opf_file = os.path.join(folder, title + ".opf")
     with open(opf_file, "w") as f:
         f.write(opf_content.strip())
+
+    return opf_file
