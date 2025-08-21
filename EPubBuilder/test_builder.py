a8cff148612b26033c607bee6824be649507797a
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 18:03:23 +0000

    Update test_builder.py

--- EPubBuilder/test_builder.py
+++ EPubBuilder/test_builder.py
@@ -2,16 +2,16 @@ import os
 
 from Builder import Builder
 
+builder = Builder("../tests/epubs")
+
 
 def test_make_container_folder():
-    builder = Builder("../tests/epubs")
     builder.make_container_folder("second_epub")
     epubs_list = os.listdir("../tests/epubs")
     assert "second_epub" in epubs_list
 
 
 def test_make_meta_inf_folder():
-    builder = Builder("../tests/epubs")
     builder.make_container_folder("third_epub")
     builder.make_meta_inf_folder(builder.epub_container)
     meta_inf_path = builder.meta_inf_folder
