bf974bbc20e17e63a9ed1bc7642a9c954d48ac61
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 18:09:05 +0000

    Update test_builder.py

--- EPubBuilder/test_builder.py
+++ EPubBuilder/test_builder.py
@@ -12,7 +12,12 @@ def test_make_container_folder():
 
 
 def test_make_meta_inf_folder():
-    builder.make_container_folder("third_epub")
+    builder.make_container_folder("test_epub")
     builder.make_meta_inf_folder(builder.epub_container)
     meta_inf_path = builder.meta_inf_folder
     assert os.path.exists(meta_inf_path)
+
+
+def test_make_opf_file():
+    builder.make_opf_file()
+    assert os.path.exists(builder.opf_file)
