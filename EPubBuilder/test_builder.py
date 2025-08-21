8fab77e1a13aff77386457fc219355eb5ca0aae2
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 12:34:54 +0000

    Update test_builder.py

--- EPubBuilder/test_builder.py
+++ EPubBuilder/test_builder.py
@@ -7,6 +7,6 @@ import functions
 
 def test_make_container_folder():
     builder = Builder("../tests/epubs")
-    _ = builder.make_container_folder("second_epub")
+    builder.make_container_folder("second_epub")
     epubs_list = os.listdir("../tests/epubs")
     assert "second_epub" in epubs_list
