30720b3cf19015dda4e531591847da5c9a4e2a17
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-21 12:34:50 +0000

    Update Builder.py

--- EPubBuilder/Builder.py
+++ EPubBuilder/Builder.py
@@ -7,8 +7,10 @@ class Builder:
 
     def __init__(self, store_folder: str = ""):
         self.store_folder = store_folder
+        self.epub_container: str = ""
+        self.meta_inf_folder: str = ""
 
-    def make_container_folder(self, name: str = "") -> str:
+    def make_container_folder(self, name: str = ""):
         """
         Creates a container folder for the EPUB structure.
 
@@ -17,4 +19,12 @@ class Builder:
         os.makedirs(container_name, exist_ok=True)
 
         make_mimetype_file(container_name)
-        return container_name
+        self.epub_container = container_name
+
+    def make_meta_inf_folder(self, container_name: str):
+        """
+        Creates the META-INF folder inside the EPUB container.
+        """
+        meta_inf_folder = os.path.join(container_name, "META-INF")
+        os.makedirs(meta_inf_folder, exist_ok=True)
+        self.meta_inf_folder = meta_inf_folder
