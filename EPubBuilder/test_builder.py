b00e1f01a33fc799abfa0e0acdb9360460c69b6f
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-08-22 15:46:14 +0000

    Update test_builder.py

--- EPubBuilder/test_builder.py
+++ EPubBuilder/test_builder.py
@@ -2,7 +2,7 @@ import os
 
 from Builder import Builder
 
-builder = Builder("../tests/epubs")
+builder = Builder(os.path.join("..", "tests", "epubs"))
 
 
 def test_make_container_folder():
@@ -21,3 +21,11 @@ def test_make_meta_inf_folder():
 def test_make_opf_file():
     builder.make_opf_file()
     assert os.path.exists(builder.opf_file)
+
+
+def test_create_template_html():
+    builder.create_template_html()
+    opf_folder = os.path.join(builder.epub_container, builder.opf_container)
+    book_path = os.path.join(opf_folder, "book.html")
+    nav_path = os.path.join(opf_folder, "nav.html")
+    assert os.path.exists(book_path) and os.path.exists(nav_path)
